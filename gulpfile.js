4bd020cd12edd6e1e1a9fcd85c0057d4b216738c
Jesse Wolgamott <jesse@comalproductions.com>
2014-09-06 11:50:19 -0500

    Add the things

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
 var deploy = require("gulp-gh-pages");
-var options = {};
 
 gulp.task('deploy', function () {
-  gulp.src("_book/**/*")
-    .pipe(deploy(options));
+    gulp.src("./_book/**/**")
+        .pipe(deploy({}))
 });
